2f933006db07c6514ee19b6cfafb70ee020d247c
sly <sylvaincadoret@hotmail.com>
2024-01-08 14:18:10 +0100

    🎉 let's start

---
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+
+function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio</h1>
+      </div>
+    );
+  }
+  
+  export default Portfolio;
---
+++ src/Pages/Skills/Skills.jsx
@@ -0,0 +1,10 @@
+
+function Competences() {
+    return (
+      <div>
+        <h1>Competences</h1>
+      </div>
+    );
+  }
+  
+  export default Competences;
