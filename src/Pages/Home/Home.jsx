3fbb40da3ef820fa6a801ee186ffd9d7c0a6ad30
sly <sylvaincadoret@hotmail.com>
2024-01-12 16:16:08 +0100

    ✅ update homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import "./style.scss";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
@@ -36,8 +38,11 @@ function Homepage() {
           </p>
 
           <p> Let's craft the future together! 💻✨</p>
-          {/* bouton CTA*/}
-          <button>Découvrez mes projets</button>
+
+          <button>
+            {" "}
+            <Link to="/portfolio">Découvrez mes projets</Link>
+          </button>
         </div>
         <div className="homepage__picture">
           <img
@@ -51,7 +56,7 @@ function Homepage() {
       {/* DEBUT SECTION 2  */}
       <div className="section_2">
         <div className="presentation_titleImg">
-          <div className="presentation_title">"Commerce, Web, Élan"</div>
+          <div className="presentation_title">Commerce, Élan, Code</div>
           <div className="presentation_img">
             <img src="./media/fusee2.png" alt="Sylvain Cadoret" />
           </div>
@@ -59,28 +64,28 @@ function Homepage() {
         <div className="presentation_text">
           <p>Bonjour à tous, </p>
           <p>
-            Directeur Adjoint de magasin en reconversion après 9 ans de
-            commerce.{" "}
+            Je suis Directeur Adjoint de magasin en reconversion après 9 ans de
+            commerce.
           </p>
           <p>
-            Passionné de nature, de voyage, de plongée, d'aquariophilie, de
-            terrariophilie et de botanique, je suis reconnu pour mon approche
-            positive et mon enthousiasme contagieux. Ma reconversion vers le
-            développement web découle de ma fascination pour ce domaine.{" "}
+            Passionné de nature, de voyage, d'aquariophilie, de terrariophilie
+            et de botanique, je suis reconnu pour mon approche positive et mon
+            enthousiasme contagieux. Ma reconversion vers le développement web
+            découle de ma fascination pour ce domaine.
           </p>
           <p>
             Aujourd'hui, je souhaite mettre à profit mes compétences au service
-            d'une entreprise dynamique.{" "}
+            d'une entreprise dynamique.
           </p>
           <p>
             Mon objectif est de contribuer à des projets web passionnants tout
-            en continuant à apprendre et à grandir professionnellement.{" "}
+            en continuant à apprendre et à grandir professionnellement.
           </p>
           <p>
             Ma combinaison d'expérience en gestion, de positivité et de mon
             attrait pour le développement web fait de moi un collaborateur
             déterminé, enthousiaste et prêt à relever de nouveaux défis. Je suis
-            impatient de contribuer au succès de votre entreprise.{" "}
+            impatient de contribuer au succès de votre entreprise.
           </p>
           <p>
             Merci de prendre le temps de découvrir mon parcours, et n'hésitez
